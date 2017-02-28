ca569c8bbf4592f701d0415251d4fbb1769c72ab
SinLucifer <543740854@qq.com>
2017-02-28 17:14:42 +0800

    Initial commit

---
+++ app/src/main/java/org/sin/ipcmessenger/MyConstants.java
@@ -0,0 +1,9 @@
+package org.sin.ipcmessenger;
+
+/**
+ * Created by Sin on 2016/7/21.
+ */
+public class MyConstants {
+    public static final int MSG_FROM_CLIENT = 0;
+    public static final int MSG_FROM_SERVICE = 1;
+}
